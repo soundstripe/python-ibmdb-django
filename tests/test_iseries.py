cc18c6494f774617a940e3ce6843a337a3b6e46b
Steven James <sjames@jacksonfurnind.com>
2019-12-03 14:24:26 -0500

    add tests for quote_value

--- tests/test_iseries.py
+++ tests/test_iseries.py
@@ -1,24 +1,30 @@
 import pytest
-from django.db import connection
 
-
-# @pytest.fixture(autouse=True)
-# def install_backend():
-#     import django.db.backends
-#     import iseries
-#     django.db.backends.iseries = iseries
-
-
-# @pytest.fixture(scope='session')
-# def django_db_createdb():
-#     return False
-#
-#
-# @pytest.fixture(scope='session')
-# def django_db_keepdb():
-#     return True
-#
 
 @pytest.mark.django_db
 def test_something():
     assert True
+
+
+@pytest.fixture
+def connection():
+    from django.db import connection
+    new_connection = connection.copy()
+    yield new_connection
+    new_connection.close()
+
+
+@pytest.fixture
+def editor(connection):
+    return connection.schema_editor()
+
+
+@pytest.mark.parametrize('value,expected', [
+    ('string', "'string'"),
+    (42, '42'),
+    (1.754, '1.754'),
+    (False, '0'),
+])
+def test_quote_value(editor, value, expected):
+    actual = editor.quote_value(value)
+    assert expected == actual
