d4a9b059f7395f1ee6f9d6a344553503f3821cc0
Steven James <sjames@jacksonfurnind.com>
2019-12-05 13:38:07 -0500

    update/reorganize database features

--- iseries/base.py
+++ iseries/base.py
@@ -42,10 +42,26 @@ dbms_name = 'dbname'
 
 
 class DatabaseFeatures(BaseDatabaseFeatures):
-    can_use_chunked_reads = True
+    allows_group_by_pk = True
+    allows_group_by_selected_pks = True
 
-    # Save point is supported by DB2.
+    interprets_empty_strings_as_nulls = False
+
+    can_use_chunked_reads = True
     uses_savepoints = True
+    can_release_savepoints = True
+
+    related_fields_match_type = False
+    has_select_for_update = True
+    has_select_for_update_skip_locked = True
+    has_select_for_update_of = True
+    select_for_update_of_column = True
+
+    test_db_allows_multiple_connections = False
+
+    supports_unspecified_pk = True
+
+    supports_forward_references = False  # ??
 
     # Custom query class has been implemented
     # django.db.backends.db2.query.query_class.DB2QueryClass
@@ -57,22 +73,19 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     supports_tablespaces = True
 
     uppercases_column_names = True
-    interprets_empty_strings_as_nulls = False
-    allows_primary_key_0 = True
+    allows_auto_pk_0 = True
     can_defer_constraint_checks = False
-    supports_forward_references = False
     requires_rollback_on_dirty_transaction = True
     supports_regex_backreferencing = True
     supports_timezones = False
     has_bulk_insert = True
-    has_select_for_update = True
+
     supports_long_model_names = False
     can_distinct_on_fields = False
-    supports_paramstyle_pyformat = False
+    supports_paramstyle_pyformat = True
     supports_sequence_reset = True
     # DB2 doesn't take default values as parameter
     requires_literal_defaults = True
-    has_case_insensitive_like = True
     can_introspect_big_integer_field = True
     can_introspect_boolean_field = False
     can_introspect_positive_integer_field = False
@@ -80,7 +93,15 @@ class DatabaseFeatures(BaseDatabaseFeatures):
     can_introspect_null = True
     can_introspect_ip_address_field = False
     can_introspect_time_field = True
+    can_rollback_ddl = True
+    has_case_insensitive_like = False
     bare_select_suffix = ' FROM SYSIBM.SYSDUMMY1'
+    supports_select_for_update_with_limit = False
+    ignores_table_name_case = True
+    supports_over_clause = True
+
+    create_test_procedure_without_params_sql = None
+    create_test_procedure_with_int_param_sql = None
 
 
 class DatabaseValidation(BaseDatabaseValidation):
